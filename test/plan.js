f26a29ca882e3246da5322ce967bf40231c01a42
Eugene Ware <eugene@noblesamurai.com>
2014-01-14 09:47:37 +1100

    don't need asserts

--- test/plan.js
+++ test/plan.js
@@ -15,15 +15,6 @@ function afterEach(num, cb) {
   });
 }
 
-var asserts = {
-  like: function (str, reg, msg) {
-    this.ok(reg.test(str), msg);
-  },
-  notlike: function (str, reg, msg) {
-    this.ok(!reg.test(str), msg);
-  }
-};
-
 var redtest = redtape({ setup: beforeEach, teardown: afterEach });
 redtest('plan: should support plan execution syntax', 3, function (t, num) {
   t.equals(num, 42);
